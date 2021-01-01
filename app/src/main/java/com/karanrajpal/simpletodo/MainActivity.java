ee6ae2649c077a90eb39a57a55b9b2d5aab73c07
Karan Rajpal <rajpal.karan73@gmail.com>
2021-01-01 13:34:37 +0530

    Added persistence

--- app/src/main/java/com/karanrajpal/simpletodo/MainActivity.java
+++ app/src/main/java/com/karanrajpal/simpletodo/MainActivity.java
@@ -5,11 +5,17 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -31,15 +37,13 @@ public class MainActivity extends AppCompatActivity {
         etTaskItem = findViewById(R.id.editTextTaskItem);
         rvTasksList = findViewById(R.id.rvTaskList);
 
-        tasks = new ArrayList<>();
-        tasks.add("Buy eggs");
-        tasks.add("Call mom");
-        tasks.add("Pay rent");
+        loadTasks();
 
         TaskItemsAdapter.OnLongClickListener longClickListener = listItemIndex -> {
             tasks.remove(listItemIndex);
             adapter.notifyItemRemoved(listItemIndex);
             Toast.makeText(getApplicationContext(), "Task removed from list", Toast.LENGTH_LONG).show();
+            saveTasks();
         };
 
         adapter = new TaskItemsAdapter(tasks, longClickListener);
@@ -52,6 +56,30 @@ public class MainActivity extends AppCompatActivity {
             adapter.notifyItemInserted(tasks.size() - 1);
             etTaskItem.setText("");
             Toast.makeText(getApplicationContext(), "Task added successfully", Toast.LENGTH_SHORT).show();
+            saveTasks();
         });
+    }
+
+    private File getDataFile() {
+        return new File(getFilesDir(), "data.txt");
+    }
+
+    private void loadTasks() {
+        try {
+            tasks = new ArrayList<>(FileUtils.readLines(getDataFile(), Charset.defaultCharset()));
+        } catch (IOException e) {
+            Log.e("MainActivity-loadTasks", "Error reading tasks", e);
+            Toast.makeText(getApplicationContext(), "Error loading tasks. Please try again later.", Toast.LENGTH_LONG).show();
+            tasks = new ArrayList<>();
+        }
+    }
+
+    private void saveTasks() {
+        try {
+            FileUtils.writeLines(getDataFile(), tasks);
+        } catch (IOException e) {
+            Log.e("MainActivity-saveTasks", "Error writing tasks", e);
+            Toast.makeText(getApplicationContext(), "Error saving tasks.", Toast.LENGTH_LONG).show();
+        }
     }
 }
